1f9d7a92539cb74b4558f09edec99a4c44fa65de
Sergio Alberto Bernal Cisneros <sergiobernal909@gmail.com>
2020-12-12 23:03:24 -0600

    server.py > Params read done

--- server.py
+++ server.py
@@ -1,20 +1,35 @@
 import SimpleHTTPServer
 import SocketServer
 import os
+import subprocess
 PORT = 8000
 
 class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
+    """
+    El formato para obtener los parametros es el siguiente:
+    "menu": 'menu_name' el identificador del menu del daemon
+    "option": 'option name' el identificador de la 
+    opcion del menu del dameon
+    """
     def do_POST(self):
         content_len = int(self.headers.getheader('content-length', 0))
         post_body = self.rfile.read(content_len)
         query_components = dict(qc.split("=") for qc in post_body.split("&"))
-        print query_components["param1"]
-        if post_body=="DIR":
-            print(os.getcwd())
-        self.wfile.write(os.getcwd())
-        self.wfile.write('\n')
+        menu = query_components['menu']
+        if menu == 'functions':
+            option = query_components['option']
+            if option == 'storage':
+                value = subprocess.check_output(['./scripts/functions.sh', option])
+            elif option == 'info':
+                value = 'info'
+            elif option == 'process':
+                value = 'process'
+            elif option == 'dirs':
+                value = 'dirs'
+        self.wfile.write(value)
         self.send_response(200)
+
 
 Handler = ServerHandler
 web_dir = os.path.join(os.path.dirname(__file__), '.')
