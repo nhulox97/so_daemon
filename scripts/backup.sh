e173cc92a86ef5aa4071285cf54d4ed1d6401ea1
Sergio Alberto Bernal Cisneros <sergiobernal909@gmail.com>
2020-12-13 16:20:34 -0600

    scripts/backup.sh > success test on backup

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 
 db_name="$1"
+receiver="$2"
 password='dosv2018'
 user='nhulox97'
+current=`pwd`
+backups_dir="$current/backups"
+logfile="$backups_dir/logfile.log"
+date=`date '+%Y%m%d %H:%M'`
+current_date=`date +'%Y-%m-%d'`
+current_time=`date +'%H:%M'`
+
+mysql=`ps awx | grep 'mysql' |grep -v grep|wc -l`
+
+if [ $mysql == 0  ]; then
+    if [[ "$(whoami)" != root ]]; then
+        echo 'Mysql esta detenido, pero el usuario no tiene permisos para reiniciar el servicio'
+        exit 1
+    else
+        service mysql restart
+        echo 'Se reinicio el servicio.'
+    fi
+fi
+
+find $backups_dir/[a-z]* -name \*.sql -mtime +3 -exec rm {} \;
+find $backups_dir/[a-z]* -name \*.tar.bz2 -mtime +3 -exec rm {} \;
+
+mysqldump --user=$user --password=$password $db_name > $backups_dir/dump-$db_name-$date.sql
+
+backup_file="backup-$db_name-$date.tar.bz2"
+cd $backups_dir && tar -cvzf $backup_file *.sql 
+
+echo "Backup Database: $db_name. Backup Date: $current_date $current_time" | mutt -a "$backup_file" -s "Backup: $db_name" -- $receiver
+cd $current
 
